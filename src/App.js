e5925692be0f8cf57cd927391833cf013fed4429
ChimaCuma <oleg.bragin.01@gmail.com>
2022-09-06 11:52:59 +0300

    donation-form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import Form from './components/form-components/Form';
 function App() {
   return (
     <div className="App">
-      
+      <Form></Form>
     </div>
   );
 }
