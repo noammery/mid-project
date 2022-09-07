e18fc64c50ed0c8a6e8625ff122013921164e279
noammery <noammery55@gmail.com>
2022-09-07 16:21:22 +0300

    modals+scroll

--- src/components/areas/South.jsx
+++ src/components/areas/South.jsx
@@ -6,9 +6,24 @@ import Typography from "@mui/material/Typography";
 import { useState } from "react";
 import { Formik } from "formik";
 import "./south.css";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
 
 const South = () => {
-  const [isSent, setIsSent] = useState(false);
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="South">
@@ -26,6 +41,7 @@ const South = () => {
           <img
             src="https://www.gov.il/BlobFolder/generalpage/beer_sheva_cyber_capital/he/%D7%91%D7%90%D7%A8%20%D7%A9%D7%91%D7%A2%20%D7%90%D7%97%D7%A8%D7%99%20600800.jpg"
             className="areaImg"
+            alt=""
           />
         </div>
       </div>
@@ -126,7 +142,7 @@ const South = () => {
                       "Are you sure you want to send this request?"
                     )
                   ) {
-                    setIsSent(true);
+                    handleOpen();
                   }
                 }} // what the submit button does
               >
@@ -163,14 +179,31 @@ const South = () => {
                         onBlur={handleBlur}
                       />
 
-                      <button
-                        className="submitButton, button-81"
-                        type="submit"
-                        role="button"
-                      >
+                      <button className="submitButton, button-81" type="submit">
                         send
                       </button>
-                      {isSent ? <div>hello</div> : <></>}
+                      <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                      >
+                        <Box sx={style}>
+                          <Typography
+                            id="modal-modal-title"
+                            variant="h6"
+                            component="h2"
+                          >
+                            Congratulations!
+                          </Typography>
+                          <Typography
+                            id="modal-modal-description"
+                            sx={{ mt: 2 }}
+                          >
+                            Your request has been sent!
+                          </Typography>
+                        </Box>
+                      </Modal>
                     </div>
                   </form>
                 )}
