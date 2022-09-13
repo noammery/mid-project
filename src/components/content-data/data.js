cfbfba42a2a4c9ff9c4874cb99b6de3b2cda24ee
ChimaCuma <oleg.bragin.01@gmail.com>
2022-09-13 08:00:31 +0300

    search data

---
+++ src/components/content-data/data.js
@@ -0,0 +1,130 @@
+exports.data = {
+    center: [
+        {
+            title: 'The association for the promotion of education in Tel Aviv',
+            details: "The association helps and works in a variety of fields for underprivileged populations and has many volunteering programs as part of it.Volunteers with a car are currently needed to distribute food collected for needy families.As part of volunteering - taking the food from the packing center located on Tel Givorim Street, and distributing it to the families inneed according to the registration.",
+            start: new Date(2022, 8, 25),
+            end: new Date(2022, 8, 29),
+        },
+        {
+            title: "The association for the promotion of education in Jaffa",
+            details: "An association operates a project to help children from disadvantaged families, through mentoring for at-risk children aged 4-12. The volunteers are asked to serve as a kind of mature and significant brother figure for the child, listening attentively and treating, helping with studies, etc.",
+            start: new Date(2022, 8, 28),
+            end: new Date(2022, 8, 30),
+        },
+        {
+            title: "Magen David Adom - Central region",
+            details: "Magen David Adom, in conjunction with the Israel Experience, has the perfect program for you. Come make a difference in Israel! Be trained as an ambulance first responder (EMS) or senior first responder. Volunteer on ambulances throughout Israel. Where else in the world can you do this? Be on the forefront of emergencies in Israel!",
+            start: new Date(2022, 8, 20),
+            end: new Date(2022, 9, 20),
+        },
+        {
+            title: '"Mabruk"',
+            details: "In short, Mabrok is an association that produces weddings for underprivileged couples. Then If you have a connection to the world of weddings, if you are looking for significant volunteering and have a crazy desire to take part in an amazing project - we want you!! Join us in the Mabruk family!",
+            start: new Date(2022, 9, 1),
+            end: new Date(2022, 9, 31),
+        },
+        {
+            title: "Singing in nursing homes",
+            details: "Come sing and make old people happy in nursing homes in the center.",
+            start: new Date(2022, 8, 27),
+            end: new Date(2022, 8, 27),
+        },
+        {
+            title: "Minimizing drug harm",
+            details: "Volunteers are needed for a special project dealing with the minimization of drug damage! A harm minimization approach is a holistic approach, which comes down from the high tree of eradicating the phenomenon of addiction and chooses to see the person as he is. Without judging or trying to fix and with a lot of compassion and an open heart. The Yizhar Center supports the people who are in the cycle of addiction and prostitution, through access to food, clothing, hot showers, clean syringes and above all through the presence of humanity for the people whose life circumstances pushed them to the margins.",
+            start: new Date(2022, 9, 7),
+            end: new Date(2022, 9, 15),
+        },
+        {
+            title: "Association to fight cancer - Givatayim",
+            details: "Volunteers are needed who are open-hearted, sensitive and willing to help with the branch's activities.",
+            start: new Date(2022, 8, 17),
+            end: new Date(2022, 8, 17),
+        },
+        {
+            title: "Food packaging for the needy",
+            details: "Food packaging of food donated to us and brought to the destination when necessary. The food is donated to people who do not have the financial ability to buy food for themselves.",
+            start: new Date(2022, 8, 21),
+            end: new Date(2022, 9, 1),
+        }
+    ],
+
+    north: [
+        {
+            title: 'Volunteering with animals - Gilboa',
+            details: 'The association works to enforce the Animal Cruelty Law and regulations by units of volunteers in the field, who handle cases of neglect and abuse while responding to requests from the public.',
+            start: new Date(2022, 8, 28),
+            end: new Date(2022, 8, 30),
+        },
+        {
+            title: 'Support for women after childbirth in Haifa',
+            details: 'Willingness to listen to the needs of the new mother, sensitivity and lack of judgment. The volunteering is short-term and is done in the free time of the supporting mother, and in the vicinity of her residence.',
+            start: new Date(2022, 9, 8),
+            end: new Date(2022, 9, 19),
+        },
+        {
+            title: 'Magen David Adom - Northen region',
+            details: 'Magen David Adom, in conjunction with the Israel Experience, has the perfect program for you. Come make a difference in Israel! Be trained as an ambulance first responder (EMS) or senior first responder. Volunteer on ambulances throughout Israel. Where else in the world can you do this? Be on the forefront of emergencies in Israel!',
+            start: new Date(2022, 9, 1),
+            end: new Date(2022, 9, 31),
+        },
+        {
+            title: 'Accompanying children, youth and adults with disabilities in cycling classes',
+            details: 'Giving cycling lessons to a variety of disabled people while being patient and supporting them.Intended only for those who ride a bicycle very well.',
+            start: new Date(2022, 9, 6),
+            end: new Date(2022, 9, 10),
+        },
+        {
+            title: 'Help with homework in "Gordon" school',
+            details: 'Play with the elderly in board games of their choice. The volunteering is in a variety of nursing homes in the north and lasts only about an hour.',
+            start: new Date(2022, 8, 19),
+            end: new Date(2022, 8, 24),
+        },
+        {
+            title: 'An hour of playing board games with the elderly',
+            details: 'Play with the elderly in board games of their choice. The volunteering is in a variety of nursing homes in the north and lasts only about an hour.',
+            start: new Date(2022, 9, 11),
+            end: new Date(2022, 9, 16),
+        }
+    ],
+
+    south: [
+        {
+            title: 'Volunteering at Soroka Hospital',
+            details: 'Helping patients by raising morale. You can bring tasty things or board games or anything you think will make the patients happy.',
+            start: new Date(2022, 8, 25),
+            end: new Date(2022, 9, 4),
+        },
+        {
+            title: 'Rehabilitation of "Darom Adom"',
+            details: 'Helping farmers in restoring the garden. Volunteering is a little physical and suitable for young people.',
+            start: new Date(2022, 8, 21),
+            end: new Date(2022, 8, 22),
+        },
+        {
+            title: 'Magen David Adom - South region',
+            details: 'Magen David Adom, in conjunction with the Israel Experience, has the perfect program for you. Come make a difference in Israel! Be trained as an ambulance first responder (EMS) or senior first responder. Volunteer on ambulances throughout Israel. Where else in the world can you do this? Be on the forefront of emergencies in Israel!',
+            start: new Date(2022, 9, 1),
+            end: new Date(2022, 9, 31),
+        },
+        {
+            title: 'Company hosting for older people',
+            details: 'Come as long as you want to host company and make them feel good.',
+            start: new Date(2022, 10, 15),
+            end: new Date(2022, 10, 15),
+        },
+        {
+            title: 'Accompanying new immigrants and their families in the community',
+            details: 'Help with the Hebrew language and hosting the families on Israeli holidays. You should introduce them to our customs and culture as much as possible.',
+            start: new Date(2022, 9, 18),
+            end: new Date(2022, 9, 27),
+        },
+        {
+            title: 'Volunteering at Kibbutz Lotan',
+            details: 'Volunteers in Lotan work in the kibbutz branches that need manpower - usually kitchen/dining room, tourism, dairy and ornamental. We accept young and healthy people.',
+            start: new Date(2022, 9, 6),
+            end: new Date(2022, 9, 23),
+        }
+    ]
+}
